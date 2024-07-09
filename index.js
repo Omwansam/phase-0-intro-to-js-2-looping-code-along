cc9d00ec96b2e0da7abb3bee6612e658ca3c61b4
Omwansam <omwansamarnold@gmail.com>
2024-07-09 11:00:16 +0300

    Looping complete lab

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // Code your solutions in this file
-function writeCards(names,event){
+  const names = ['Guadalupe', 'Ollie', 'Aki'];
+  const eventName = 'surprise';
+  
+function writeCards(names,eventName){
     let new_arr=[]
     for (let i=0; i < names.length; i++){
-        new_arr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+        new_arr.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
     }
 
     return new_arr
